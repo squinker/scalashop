8fe9e577d9e2857a19719c0edfad07be3060deab
William Bellamy <william.bellamy@itv.co.uk>
2017-02-23 09:48:14 +0000

    implement parblur

--- src/main/scala/scalashop/HorizontalBoxBlur.scala
+++ src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -1,7 +1,7 @@
 package scalashop
 
-import org.scalameter._
 import common._
+import org.scalameter._
 
 object HorizontalBoxBlurRunner {
 
@@ -10,7 +10,7 @@ object HorizontalBoxBlurRunner {
     Key.exec.maxWarmupRuns -> 10,
     Key.exec.benchRuns -> 10,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val radius = 3
@@ -37,67 +37,60 @@ object HorizontalBoxBlurRunner {
 object HorizontalBoxBlur {
 
   /** Blurs the rows of the source image `src` into the destination image `dst`,
-   *  starting with `from` and ending with `end` (non-inclusive).
-   *
-   *  Within each row, `blur` traverses the pixels by going from left to right.
-   */
+    * starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each row, `blur` traverses the pixels by going from left to right.
+    */
   def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
-  // TODO implement this method using the `boxBlurKernel` method
+    // TODO implement this method using the `boxBlurKernel` method
 
-    if (debug) {println(s"Vertical bb called with from: $from, end: $end for source image of height ${src.height} , width ${src.width}") }
-    if (debug) {println(s"Destination image has height ${dst.height} and width ${dst.width}")}
-
-    /*
-    var curCol = from
-    var curRow = 0
-
-
-    while(curCol < end){
-      curRow = 0
-      while(curRow < src.height){
-
-        val blurredRGBA = boxBlurKernel(src, curCol, curRow, radius)
-        try {
-          dst.update((curCol), curRow, blurredRGBA)
-        }
-        catch{
-          case e: Throwable => println(s"Errored trying to update img (height:${dst.height}, width: ${dst.width} at curCol:$curCol, curRow:$curRow")
-            throw e
-        }
-        curRow += 1
-      }
-      curCol += 1
+    if (debug) {
+      println(s"Vertical bb called with from: $from, end: $end for source image of height ${src.height} , width ${src.width}")
     }
-  */
+    if (debug) {
+      println(s"Destination image has height ${dst.height} and width ${dst.width}")
+    }
+
     var curRow = from
     var curCol = 0
 
-    while(curRow < end) {
+    while (curRow < end) {
       curCol = 0
-       while(curCol < src.width) {
+      while (curCol < src.width) {
 
-         val blurredRGBA = boxBlurKernel(src, curCol, curRow, radius)
-         dst.update(curCol, curRow, blurredRGBA)
-
-         curCol += 1
-       }
-      curRow +=1
+        val blurredRGBA = boxBlurKernel(src, curCol, curRow, radius)
+        dst.update(curCol, curRow, blurredRGBA)
+        curCol += 1
+      }
+      curRow += 1
     }
-
-
-
   }
 
   /** Blurs the rows of the source image in parallel using `numTasks` tasks.
-   *
-   *  Parallelization is done by stripping the source image `src` into
-   *  `numTasks` separate strips, where each strip is composed of some number of
-   *  rows.
-   */
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * rows.
+    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-  // TODO implement using the `task` construct and the `blur` method
-
-  ???
+    // TODO implement using the `task` construct and the `blur` method
+    val starts = (0 to src.height).by(numTasks).toList
+    starts.zip(starts.tail).map { startEnd =>
+      val t = task(blur(src, dst, startEnd._1, startEnd._2, radius))
+      t.join
+    }
   }
+
+  /*
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    // TODO implement using the `task` construct and the `blur` method
+    val starts = (0 to src.width).by(numTasks).toList
+    starts.zip(starts.tail).map { startEnd =>
+      val t = task( blur(src, dst, startEnd._1, startEnd._2, radius) )
+      t.join
+    }
+  }
+
+   */
 
 }
